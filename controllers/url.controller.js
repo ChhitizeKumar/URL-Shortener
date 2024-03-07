f61bf333dad1591867d8f7d27107ad7833735c61
ChhitizeKumar <chhitizeathghara11@gmail.com>
2024-03-08 03:23:32 +0530

    add url-analytics endpoint

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -1,27 +1,36 @@
 import { ytid } from "ytid";
 import { Url } from "../models/url.model.js";
 
-const generateShortUrl = async(req,res) => {
+const generateShortUrl = async (req, res) => {
+  const { url } = req.body;
 
-    const {url} = req.body;
-
-    if(!url)
-    {
-        return res.status(400).json({
-            error: "Url is required"
-        })
-    }
-    const shortId = ytid();
-
-    const shorterenUrl = await Url.create({
-      shortId: shortId,
-      redirectURL: url,
-      visitHistory: [],
+  if (!url) {
+    return res.status(400).json({
+      error: "Url is required",
     });
+  }
+  const shortId = ytid();
 
-    return res.status(200).json({
-        id: shortId
-    })
-} 
+  const shorterenUrl = await Url.create({
+    shortId: shortId,
+    redirectURL: url,
+    visitHistory: [],
+  });
 
-export {generateShortUrl,}
+  return res.status(200).json({
+    id: shortId,
+  });
+};
+
+const handelUrlAnalytics = async (req, res) => {
+  const shortId = req.params.shortId;
+
+  const result = await Url.findOne({ shortId });
+
+  return res.status(200).json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+};
+
+export { generateShortUrl, handelUrlAnalytics };
--- routes/url.routes.js
+++ routes/url.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { generateShortUrl } from "../controllers/url.controller.js";
+import { generateShortUrl, handelUrlAnalytics } from "../controllers/url.controller.js";
 
 const router = Router();
 
 router.route("/").post(generateShortUrl);
+
+router.route("/analytics/:shortId").get(handelUrlAnalytics)
 
 export default router;
